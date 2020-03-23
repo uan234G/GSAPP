d41b33a9e3a83f93a7046faad7f69cb865502b8a
juan gonzalez <jcgonzalez.t3@gmail.com>
2020-03-23 13:49:17 -0700

    initial commit

---
+++ Models/Task.cs
@@ -0,0 +1,33 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GSAPP.Models {
+    public class Task {
+        [Key]
+        public int TaskID { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+        // 1.groceries
+        // 2.medication
+        // 3.other
+        [Required]
+        public string Items { get; set; }
+        // user can write down everything they need in textbox
+
+        [Required]
+        public string Urgency { get; set; }
+        // 1. low
+        // 2. normal
+        // 3. critical
+        [Required]
+        public bool IsCompleted { get; set; }
+        // default val = false;
+        public int PickedUpByID { get; set; }
+        // user picking up task/ getting id instead of name to connect to db
+        public DateTime CreatedAt = DateTime.Now;
+        public DateTime UpdatedAt = DateTime.Now;
+        public int UserID { get; set; }
+        public User Creator { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GSAPP.Models {
+    public class User {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required (ErrorMessage = "Enter a name")]
+        [MinLength (2, ErrorMessage = "Name should be more than 2 characters")]
+        public string FirstName { get; set; }
+
+        [Required (ErrorMessage = "Enter a last name")]
+        [MinLength (2, ErrorMessage = "Last name should be more than 2 characters")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+        // upon registering you can choose Helper or Person in need of help??
+
+        [Required]
+        [EmailAddress]
+        [MinLength (4, ErrorMessage = "Email address should be more than 4 characters")]
+        [RegularExpression (@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType (DataType.Password)]
+        [MinLength (8, ErrorMessage = "Password must be at least 8 characters")]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Compare ("Password")]
+        [DataType (DataType.Password)]
+        public string Confirm { get; set; }
+
+        public virtual Address Location { get; set; }
+        // joining one to one to adress 
+        // see  adress model
+        public DateTime CreatedAt = DateTime.Now;
+        public DateTime UpdatedAt = DateTime.Now;
+        public List<Task> TasksCreated { get; set; }
+        // one to many ralationship to task model..
+    }
+}
+
+// Hello world!! - Rolando
+// juan was here

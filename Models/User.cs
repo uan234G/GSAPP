360716bfa5928f4b4de54f3ff28a4414306d3cbc
juan gonzalez <jcgonzalez.t3@gmail.com>
2020-03-23 14:18:52 -0700

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace GSAPP.Models {
-    public class User {
+namespace GSAPP.Models
+{
+    public class User
+    {
         [Key]
         public int UserId { get; set; }
 
-        [Required (ErrorMessage = "Enter a name")]
-        [MinLength (2, ErrorMessage = "Name should be more than 2 characters")]
+        [Required(ErrorMessage = "Enter a name")]
+        [MinLength(2, ErrorMessage = "Name should be more than 2 characters")]
         public string FirstName { get; set; }
 
-        [Required (ErrorMessage = "Enter a last name")]
-        [MinLength (2, ErrorMessage = "Last name should be more than 2 characters")]
+        [Required(ErrorMessage = "Enter a last name")]
+        [MinLength(2, ErrorMessage = "Last name should be more than 2 characters")]
         public string LastName { get; set; }
 
         [Required]
@@ -22,18 +24,18 @@ namespace GSAPP.Models {
 
         [Required]
         [EmailAddress]
-        [MinLength (4, ErrorMessage = "Email address should be more than 4 characters")]
-        [RegularExpression (@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", ErrorMessage = "Please enter a valid email address")]
+        [MinLength(4, ErrorMessage = "Email address should be more than 4 characters")]
+        [RegularExpression(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
 
         [Required]
-        [DataType (DataType.Password)]
-        [MinLength (8, ErrorMessage = "Password must be at least 8 characters")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
         public string Password { get; set; }
 
         [NotMapped]
-        [Compare ("Password")]
-        [DataType (DataType.Password)]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
         public string Confirm { get; set; }
 
         public virtual Address Location { get; set; }
@@ -43,6 +45,17 @@ namespace GSAPP.Models {
         public DateTime UpdatedAt = DateTime.Now;
         public List<Task> TasksCreated { get; set; }
         // one to many ralationship to task model..
+    }
+    [NotMapped]
+    public class Login
+    {
+        [Required]
+        [EmailAddress]
+        public string LoginEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string LoginPassword { get; set; }
     }
 }
 

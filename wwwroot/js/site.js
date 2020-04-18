d84ce42398f28a279967a1cfd5f43c1a21122764
Rolando Lopantzi <rlopantzi@gmail.com>
2020-04-17 19:00:34 -0700

    init commit

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,11 +1,13 @@
 ﻿const pwdInput = document.getElementById('password');
 const show = document.getElementById('show-pwd');
-const openNav = document.getElementById('open-nav');
-const closeNav = document.getElementById('close-btn');
-const mobileNav = document.querySelector('.mobile-nav');
-const openDashNav = document.getElementById('open-dashNav');
-const closeDashNav = document.getElementById('close-dashNav');
+
 const mobileDashNav = document.querySelector('.mobile-dashNav');
+const dashNavBtn = document.getElementById('dash-btn');
+
+const mobileNav = document.querySelector('.mobile-nav');
+const navBtn = document.getElementById('nav-btn');
+
+const menuIcon = document.getElementById('menu-icon');
 
 function showPwd() {
     if (pwdInput.type === 'password'){
@@ -16,46 +18,22 @@ function showPwd() {
         pwdInput.type = 'password';
     }
 }
-function openMenu(){
-    mobileNav.classList.add('show');
-}
-function closeMenu(){
-    mobileNav.classList.remove('show');
-}
-function openDash(){
-    mobileDashNav.classList.add('show');
-}
-function closeDash(){
-    mobileDashNav.classList.remove('show');
+
+navBtn.addEventListener('click', landingNav);
+dashNavBtn.addEventListener('click', dashNav);
+
+function landingNav(){
+    console.log('clicked');
+    menuIcon.classList.toggle('animate')
+    mobileNav.classList.toggle('show');
 }
 
-Array.prototype.forEach.call(
-    document.querySelectorAll(".upload-btn"),
-    function (button) {
-        const hiddenInput = button.parentElement.querySelector(
-            ".real-file"
-        );
+function dashNav(){
+    console.log('clicked dash');
+    menuIcon.classList.toggle('animate')
+    mobileDashNav.classList.toggle('show');
+}
 
-        const label = button.parentElement.querySelector(".file-callout");
-        const defaultLabelText = "No file selected";
-
-        label.textContent = defaultLabelText;
-        label.title = defaultLabelText;
-
-        button.addEventListener("click", function () {
-            hiddenInput.click();
-        });
-        hiddenInput.addEventListener("change", function () {
-            const filenameList = Array.prototype.map.call(hiddenInput.files, function (
-                file
-            ) {
-                return file.name;
-            });
-            label.textContent = filenameList.join(", ") || defaultLabelText;
-            label.title = label.textContent;
-        });
-    }
-);
 
 window.onload = function () {
     getCovidStats();
